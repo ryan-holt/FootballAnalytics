487020c0fa14cc621143a0d1d7bcdb3616df06c5
harsohailbrar <harsohail.brar1@gmail.com>
2020-02-24 12:57:02 -0700

    create admin endpoint

---
+++ resources/admin.py
@@ -0,0 +1,61 @@
+import MySQLdb
+from flask import request, jsonify
+from flask_restplus import Resource, fields
+
+from restplus import api, db
+
+ns = api.namespace('admins', description='Operations related to admins')
+admin = api.model('Admin',
+                  {'username': fields.String(description='Username of admin', required=True, max_length=20),
+                   'password': fields.String(description='Password of admin', required=True, max_length=20),
+                   'permission_level': fields.Integer(description='Permission level given to admin', required=True)})
+
+@ns.route('/')
+class AdminList(Resource):
+    def get(self):
+        with db.engine.raw_connection().cursor(MySQLdb.cursors.DictCursor) as cursor:
+            cursor.callproc("getAdmins")
+            results = cursor.fetchall()
+        return jsonify(results)
+
+    @api.expect(admin, validate=True)
+    def post(self):
+        data = request.json
+        name = data.get('username')
+        password = data.get('password')
+        permission_level = data.get('permission_level')
+        connection = db.engine.raw_connection()
+        try:
+            with connection.cursor(MySQLdb.cursors.DictCursor) as cursor:
+                cursor.callproc("addAdmin", [name, password, permission_level])
+                connection.commit()
+        finally:
+            connection.close()
+        return {'message': 'client has been created successfully.'}, 201
+
+@ns.route('/<string:username>')
+class Admin(Resource):
+    def get(self, username):
+        if username:
+            with db.engine.raw_connection().cursor(MySQLdb.cursors.DictCursor) as cursor:
+                cursor.callproc("getAdminByUsername", [username])
+                results = cursor.fetchall()
+            return jsonify(results)
+
+    @api.expect(admin, validate=True)
+    def put(self, username):
+        data = request.json
+        new_username = data.get('username')
+        password = data.get('password')
+        permission_level = data.get('permission_level')
+        connection = db.engine.raw_connection()
+        try:
+            with connection.cursor(MySQLdb.cursors.DictCursor) as cursor:
+                cursor.callproc("updateAdminUsername", [username, new_username, password, permission_level])
+                # TODO find way to see if it actually updated anyting in the query
+                connection.commit()
+        except:
+            return {'message': 'failed to update client username.'}, 201
+        finally:
+            connection.close()
+        return {'message': 'client username has been updated successfully.'}, 201
