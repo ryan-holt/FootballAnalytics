24a5e9ed268086380f7279d5281144e23b0edf20
harsohailbrar <harsohail.brar1@gmail.com>
2020-02-24 17:01:18 -0700

    fix sql file for diff between mac and windows

--- restfulexample.sql
+++ restfulexample.sql
@@ -1,19 +1,21 @@
--- MySQL dump 10.13  Distrib 8.0.18, for macos10.14 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `restfulexample` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `restfulexample`;
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: restfulexample
+-- Host: localhost    Database: restfulexample
 -- ------------------------------------------------------
--- Server version	8.0.18
+-- Server version	8.0.15
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
-/*!40103 SET TIME_ZONE = '' + 00:00'' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = ''NO_AUTO_VALUE_ON_ZERO'' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `admin`
@@ -52,17 +54,14 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `client`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `client`
-(
-    `username`  varchar(20) NOT NULL,
-    `password`  varchar(20) NOT NULL,
-    `join_date` datetime DEFAULT NULL,
-    PRIMARY KEY (`username`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `client` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `join_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,211 +69,109 @@ CREATE TABLE `client`
 --
 
 LOCK TABLES `client` WRITE;
-/*!40000 ALTER TABLE `client`
-    DISABLE KEYS */;
-INSERT INTO `client`
-VALUES (''1'', ''john'', NULL),
-       (''2'', ''sally'', NULL),
-       (''5'', ''Ryan'', NULL),
-       (''7'', ''tyler'', NULL),
-       (''8'', ''cool'', NULL),
-       (''9'', ''gary'', NULL),
-       (''harsohailB'', ''pass'', ''2020-02-24 11:57:24''),
-       (''hello2'', ''world'', ''2020-02-24 02:17:01'');
-/*!40000 ALTER TABLE `client`
-    ENABLE KEYS */;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES ('1','john',NULL),('2','sally',NULL),('5','Ryan',NULL),('7','tyler',NULL),('8','cool',NULL),('9','gary',NULL),('hello2','world','2020-02-24 02:17:01');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping routines for database ''restfulexample''
+-- Dumping routines for database 'restfulexample'
 --
-/*!50003 DROP PROCEDURE IF EXISTS `addAdmin` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_0900_ai_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = ''ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'' */;
-DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` PROCEDURE `addAdmin`(IN username VARCHAR(20),
-                                                      IN password VARCHAR(20),
-                                                      IN permission_level Integer)
-BEGIN
-    INSERT INTO admin values (username, password, permission_level);
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
 /*!50003 DROP PROCEDURE IF EXISTS `addClient` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_0900_ai_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = ''STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'' */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` PROCEDURE `addClient`(IN username VARCHAR(20),
-                                                       IN password VARCHAR(20))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addClient`(
+	IN username VARCHAR(20),
+    IN password VARCHAR(20)
+    )
 BEGIN
-    INSERT INTO client values (username, password, current_timestamp());
+INSERT INTO client values(username, password, current_timestamp());
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
-/*!50003 DROP PROCEDURE IF EXISTS `getAdminByUsername` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_0900_ai_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = ''ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'' */;
-DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` PROCEDURE `getAdminByUsername`(
-    IN username VARCHAR(20)
-)
-BEGIN
-    SELECT * FROM admin WHERE admin.username = username;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
-/*!50003 DROP PROCEDURE IF EXISTS `getAdmins` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_0900_ai_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = ''ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'' */;
-DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` PROCEDURE `getAdmins`()
-BEGIN
-    SELECT * FROM admin;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `getClientByUsername` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_0900_ai_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = ''STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'' */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` PROCEDURE `getClientByUsername`(
-    IN username VARCHAR(20)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getClientByUsername`(
+	IN username VARCHAR(20)
 )
 BEGIN
-    SELECT * FROM client WHERE client.username = username;
+SELECT * FROM client WHERE client.username = username;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `getClients` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_0900_ai_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = ''STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'' */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` PROCEDURE `getClients`()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getClients`()
 BEGIN
-    SELECT * FROM client;
+SELECT * FROM client;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
-/*!50003 DROP PROCEDURE IF EXISTS `updateAdminUsername` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_0900_ai_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = ''ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'' */;
-DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` PROCEDURE `updateAdminUsername`(IN old_username VARCHAR(20),
-                                                                 IN new_username VARCHAR(20),
-                                                                 IN password VARCHAR(20),
-                                                                 IN permission_level Integer)
-BEGIN
-    UPDATE admin
-    SET admin.username = new_username
-    where admin.username = old_username
-      and admin.password = password
-      and admin.permission_level = permission_level;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `updateClientUsername` */;
-/*!50003 SET @saved_cs_client = @@character_set_client */;
-/*!50003 SET @saved_cs_results = @@character_set_results */;
-/*!50003 SET @saved_col_connection = @@collation_connection */;
-/*!50003 SET character_set_client = utf8mb4 */;
-/*!50003 SET character_set_results = utf8mb4 */;
-/*!50003 SET collation_connection = utf8mb4_0900_ai_ci */;
-/*!50003 SET @saved_sql_mode = @@sql_mode */;
-/*!50003 SET sql_mode = ''STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'' */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE
-    DEFINER = `root`@`localhost` PROCEDURE `updateClientUsername`(IN old_username VARCHAR(20),
-                                                                  IN new_username VARCHAR(20),
-                                                                  IN password VARCHAR(20))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updateClientUsername`(
+	IN old_username VARCHAR(20),
+    IN new_username VARCHAR(20),
+    IN password VARCHAR(20)
+)
 BEGIN
-    UPDATE client
-    SET client.username = new_username
-    where client.username = old_username
-      and client.password = password;
+UPDATE client
+SET client.username = new_username
+where client.username = old_username and client.password = password;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode = @saved_sql_mode */;
-/*!50003 SET character_set_client = @saved_cs_client */;
-/*!50003 SET character_set_results = @saved_cs_results */;
-/*!50003 SET collation_connection = @saved_col_connection */;
-/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-24 12:55:05
+-- Dump completed on 2020-02-24  2:49:11
